fccd6cc524172125362315e9ad33044e513b2fcc
Mike Rosseel <mike.rosseel@gmail.com>
2019-07-25 09:20:47 +0200

    use while loop

--- scale.py
+++ scale.py
@@ -13,30 +13,31 @@ blescan.hci_enable_le_scan(sock)
 measured_anterior = 0
 
 def get_latest_weight():
-    try:
-        returnedList = blescan.parse_events(sock, 1)
-        print(returnedList)
-        if len(returnedList) > 0:
-            (mac, uuid, major, minor, txpower, rssi) = returnedList[0].split(',', 6)
-            # CAMBIAR LA DIRECCION MAC
-            if mac == 'F4:AD:91:94:AB:36' and uuid[0:22] == '01880f1096ab190d161d18':
-                measunit = uuid[22:24]
-                measured = int((uuid[26:28] + uuid[24:26]), 16) * 0.01
+    while True:
+        try:
+            returnedList = blescan.parse_events(sock, 1)
+            print(returnedList)
+            if len(returnedList) > 0:
+                (mac, uuid, major, minor, txpower, rssi) = returnedList[0].split(',', 6)
+                # CAMBIAR LA DIRECCION MAC
+                if mac == 'F4:AD:91:94:AB:36' and uuid[0:22] == '01880f1096ab190d161d18':
+                    measunit = uuid[22:24]
+                    measured = int((uuid[26:28] + uuid[24:26]), 16) * 0.01
 
-                unit = ''
+                    unit = ''
 
-                if measunit.startswith(('03', 'b3')): unit = 'lbs'
-                if measunit.startswith(('12', 'b2')): unit = 'jin'
-                if measunit.startswith(('22', 'a2')): unit = 'Kg' ; measured = measured / 2
+                    if measunit.startswith(('03', 'b3')): unit = 'lbs'
+                    if measunit.startswith(('12', 'b2')): unit = 'jin'
+                    if measunit.startswith(('22', 'a2')): unit = 'Kg' ; measured = measured / 2
 
-                if unit:
-                    if measured != measured_anterior:
-                        print("measured : %s %s" % (measured, unit))
-                        measured_anterior = measured
-                return measured
+                    if unit:
+                        if measured != measured_anterior:
+                            print("measured : %s %s" % (measured, unit))
+                            measured_anterior = measured
+                    return measured
 
-    except KeyboardInterrupt:
-        sys.exit(1)
+        except KeyboardInterrupt:
+            sys.exit(1)
 
 
 
