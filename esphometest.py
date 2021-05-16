37376bf8e888374c41f7f27fa87f4af88528bf32
Mike Rosseel <mike.rosseel@gmail.com>
2021-05-16 19:50:15 +0200

    making html prettier, add weight filter, trying websocket error handling

--- esphometest.py
+++ esphometest.py
@@ -5,7 +5,7 @@ import random
 import datetime
 import json
 import planet_weight
-
+import math
 # pip install websocket
 # pip install aioesphomeapi (maybe)
 
@@ -13,8 +13,10 @@ import planet_weight
 def get_now():
     return datetime.datetime.now()
 
+weight_key = 2691377687
 weight = 0
 weight_date = get_now()
+planet_weight_zero = planet_weight.get_weight_json(weight)
 
 async def main():
     """Connect to an ESPHome device and get details."""
@@ -32,34 +34,43 @@ async def main():
         global weight
         global weight_date
         """Print the state changes of the device.."""
-        print(state)
-        if state is not None:
-            print("state.state:", state.state)
-            # if state.missing_state:
-            #     weight = 0
-            if state.state > 0:
-                weight_date = get_now()
-                weight = state.state
-            else:
-                time_diff = (get_now() - weight_date).total_seconds()
-                if time_diff > 10:
-                    weight = 0
+        print(f"state: {state}, isWeight:{state.key == weight_key}, state isnan: {math.isnan(state.state)}")
+        if state is not None and not math.isnan(state.state) and state.key == weight_key:
+            print("Setting weigth to ", state.state)
+            weight_date = get_now()
+            weight = state.state
 
     # Subscribe to the state changes
     await api.subscribe_states(change_callback)
 
+
+def check_stale_weight(w, w_zero, w_date):
+    time_diff = (get_now() - weight_date).total_seconds()
+    if time_diff > 30:
+       return w_zero
+    return w
+
 async def weight_socket(websocket, path):
     global weight
-    while True:
-        # print(f"Sending type: {type(weight)}, weight: {weight}")
-        await websocket.send(planet_weight.get_weight_json(weight))
-        await asyncio.sleep(1)
+    global weight_date
+    await websocket.send(check_stale_weight(planet_weight.get_weight_json(weight), planet_weight_zero, weight_date))
+    try:
+        while True:
+            try:
+                # print(f"Sending type: {type(weight)}, weight: {weight}")
+                await websocket.send(check_stale_weight(planet_weight.get_weight_json(weight), planet_weight_zero, weight_date))
+                await asyncio.sleep(1)
+            except Exception as e:
+                print("Websocket error", e)
+    finally:
+        print("exiting weight socket")
 
 start_server = websockets.serve(weight_socket, "127.0.0.1", 5678)
 
 loop = asyncio.get_event_loop()
 try:
-    asyncio.ensure_future(main())
+    # asyncio.ensure_future(main())
+    loop.create_task(main())
     loop.run_until_complete(start_server)
     loop.run_forever()
 except KeyboardInterrupt:
